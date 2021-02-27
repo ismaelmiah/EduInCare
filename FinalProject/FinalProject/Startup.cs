ce24dec88f43294690470cf39ad8429ce3847d98
Ismail5g <ismail27.dec@gmail.com>
2021-02-27 12:48:29 +0600

    Admin Area Setup & Worked

--- FinalProject/FinalProject/Startup.cs
+++ FinalProject/FinalProject/Startup.cs
@@ -78,6 +78,11 @@ namespace FinalProject
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
