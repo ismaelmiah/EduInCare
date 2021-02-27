ab462069ab71283ab7117374389b9852ac4c0319
Ismail5g <ismail27.dec@gmail.com>
2021-02-27 14:46:37 +0600

    UnitOfWork & Repository Pattern Initialized

---
+++ FinalProject/DataAccessLayer/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+        public UnitOfWork(DbContext dbContext) => _dbContext = dbContext;
+
+        public void Dispose() => _dbContext?.Dispose();
+        public void Save() => _dbContext?.SaveChanges();
+    }
+}
