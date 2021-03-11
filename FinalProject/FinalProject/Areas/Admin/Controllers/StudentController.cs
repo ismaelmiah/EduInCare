e915ca9b6e3f0b793f9f852fa2be4476a02307ce
Ismail Hossen <ismail96dream@gmail.com>
2021-03-11 22:17:06 +0600

    Action Declared

---
+++ FinalProject/FinalProject/Areas/Admin/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FinalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upsert()
+        {
+            return View();
+        }
+        public IActionResult Delete(Guid id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult StudentReport(Guid id)
+        {
+            return View();
+        }
+    }
+}
