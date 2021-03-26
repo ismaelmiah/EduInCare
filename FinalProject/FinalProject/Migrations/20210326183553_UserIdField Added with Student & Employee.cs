3f00d21dea43ba395333731687876573b998271a
Ismail Hossen <ismail96dream@gmail.com>
2021-03-27 00:38:43 +0600

    Everything Working Correctly

---
+++ FinalProject/FinalProject/Migrations/20210326183553_UserIdField Added with Student & Employee.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalProject.Web.Migrations
+{
+    public partial class UserIdFieldAddedwithStudentEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_CourseId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Students",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Employees",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                table: "Courses",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "Courses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GroupId",
+                table: "Courses",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HaveCompulsorySubject",
+                table: "Courses",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxCompulsorySubject",
+                table: "Courses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Courses",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SubjectId",
+                table: "Courses",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "AcademicYears",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    IsOpenForAdmission = table.Column<bool>(nullable: false),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AcademicYears", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Capacity = table.Column<int>(nullable: false),
+                    CourseId = table.Column<Guid>(nullable: false),
+                    TeacherId = table.Column<Guid>(nullable: false),
+                    Status = table.Column<bool>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sections_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Sections_Employees_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Code = table.Column<string>(nullable: true),
+                    CourseId = table.Column<Guid>(nullable: false),
+                    ExcludeInResult = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RegistrationNo = table.Column<string>(nullable: true),
+                    StudentId = table.Column<Guid>(nullable: false),
+                    CourseId = table.Column<Guid>(nullable: false),
+                    SectionId = table.Column<Guid>(nullable: false),
+                    AcademicYearId = table.Column<Guid>(nullable: false),
+                    ShiftId = table.Column<Guid>(nullable: false),
+                    RollNo = table.Column<int>(nullable: false),
+                    CardNo = table.Column<string>(nullable: true),
+                    BoardRegistrationNo = table.Column<string>(nullable: true),
+                    IsPromoted = table.Column<bool>(nullable: false),
+                    OldRegistrationId = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Registrations_AcademicYears_AcademicYearId",
+                        column: x => x.AcademicYearId,
+                        principalTable: "AcademicYears",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Sections_SectionId",
+                        column: x => x.SectionId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Shifts_ShiftId",
+                        column: x => x.ShiftId,
+                        principalTable: "Shifts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_GroupId",
+                table: "Courses",
+                column: "GroupId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_SubjectId",
+                table: "Courses",
+                column: "SubjectId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_AcademicYearId",
+                table: "Registrations",
+                column: "AcademicYearId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_CourseId",
+                table: "Registrations",
+                column: "CourseId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_SectionId",
+                table: "Registrations",
+                column: "SectionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_ShiftId",
+                table: "Registrations",
+                column: "ShiftId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_StudentId",
+                table: "Registrations",
+                column: "StudentId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_CourseId",
+                table: "Sections",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_TeacherId",
+                table: "Sections",
+                column: "TeacherId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Groups_GroupId",
+                table: "Courses",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Subjects_SubjectId",
+                table: "Courses",
+                column: "SubjectId",
+                principalTable: "Subjects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Groups_GroupId",
+                table: "Courses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Subjects_SubjectId",
+                table: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Registrations");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "AcademicYears");
+
+            migrationBuilder.DropTable(
+                name: "Sections");
+
+            migrationBuilder.DropTable(
+                name: "Shifts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_GroupId",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_SubjectId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "GroupId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "HaveCompulsorySubject",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "MaxCompulsorySubject",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourseId",
+                table: "Students",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DepartmentId",
+                table: "Courses",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CourseId",
+                table: "Students",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
